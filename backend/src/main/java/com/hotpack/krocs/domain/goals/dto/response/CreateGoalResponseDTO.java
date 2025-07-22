48ab14c9d13e37ce84d9264f902f55135fb77c16
Cycrypto <jh01love00@gmail.com>
2025-07-23 00:19:12 +0900

    feat: 목표 DTO 생성

---
+++ backend/src/main/java/com/hotpack/krocs/domain/goals/dto/response/CreateGoalResponseDTO.java
@@ -0,0 +1,40 @@
+package com.hotpack.krocs.domain.goals.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.hotpack.krocs.common.entity.BaseTimeEntity;
+import com.hotpack.krocs.common.entity.Priority;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CreateGoalResponseDTO{
+    private final Long goalId;
+
+    private final String title;
+
+    private final Priority priority;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private final LocalDate startDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private final LocalDate endDate;
+
+    private final Integer duration;
+
+    private final boolean completed;
+
+    private final List<SubGoalResponseDTO> subGoals;
+
+    private final Integer completionPercentage;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private final LocalDateTime createdAt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private final LocalDateTime updatedAt;
+}
