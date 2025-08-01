6dcabae90b6ac47f637cbfc55aac73c7489d35e2
yeo-li <parkseongyeol2110@gmail.com>
2025-08-01 16:38:47 +0900

    [feat] Template Entity updateFrom() 메서드 구현(#60)

--- backend/src/main/java/com/hotpack/krocs/domain/templates/domain/Template.java
+++ backend/src/main/java/com/hotpack/krocs/domain/templates/domain/Template.java
@@ -1,13 +1,24 @@
 package com.hotpack.krocs.domain.templates.domain;
 
+import com.hotpack.krocs.domain.templates.dto.request.TemplateUpdateRequestDTO;
 import com.hotpack.krocs.global.common.entity.BaseTimeEntity;
 import com.hotpack.krocs.global.common.entity.Priority;
-import com.hotpack.krocs.global.common.entity.RepeatType;
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.time.LocalDateTime;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "templates")
@@ -17,22 +28,38 @@ import java.util.List;
 @Builder
 public class Template extends BaseTimeEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "template_id")
-    private Long templateId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "template_id")
+  private Long templateId;
 
-    @Column(name = "title", nullable = false, length = 200)
-    private String title;
+  @Column(name = "title", nullable = false, length = 200)
+  private String title;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "priority", nullable = false, length = 10)
-    @Builder.Default
-    private Priority priority = Priority.MEDIUM;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "priority", nullable = false, length = 10)
+  @Builder.Default
+  private Priority priority = Priority.MEDIUM;
 
-    @Column(name = "duration", nullable = false)
-    private Integer duration;
+  @Column(name = "duration", nullable = false)
+  private Integer duration;
 
-    @OneToMany(mappedBy = "template", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<SubTemplate> subTemplates;
+  @OneToMany(mappedBy = "template", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+  private List<SubTemplate> subTemplates;
+
+  public void updateFrom(TemplateUpdateRequestDTO requestDTO) {
+
+    if (requestDTO.getTitle() != null) {
+      this.title = requestDTO.getTitle();
+    }
+
+    if (requestDTO.getDuration() != null) {
+      this.duration = requestDTO.getDuration();
+    }
+
+    if (requestDTO.getPriority() != null) {
+      this.priority = requestDTO.getPriority();
+    }
+    
+  }
 }
