340951fc11e486081cc900eaf560e7f84c1aa2fe
yeo-li <parkseongyeol2110@gmail.com>
2025-08-03 02:44:03 +0900

    [feat] SubTemplateRequest/ResponseDTO 작성(#40)

--- backend/src/main/java/com/hotpack/krocs/domain/templates/dto/response/SubTemplateResponseDTO.java
+++ backend/src/main/java/com/hotpack/krocs/domain/templates/dto/response/SubTemplateResponseDTO.java
@@ -1,6 +1,8 @@
 package com.hotpack.krocs.domain.templates.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,7 +12,17 @@ import lombok.Getter;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SubTemplateResponseDTO {
 
-    private final Long subTemplateId;
+  @JsonProperty("sub_template_id")
+  private final Long subTemplateId;
 
-    private final String title;
+  @JsonProperty("template_id")
+  private final Long templateId;
+
+  private final String title;
+
+  @JsonProperty("created_at")
+  private final LocalDateTime createdAt;
+
+  @JsonProperty("updated_at")
+  private final LocalDateTime updatedAt;
 }
