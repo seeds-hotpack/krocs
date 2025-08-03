889ffef260337a6fd8840bb5868e522ec1b3abc3
yeo-li <parkseongyeol2110@gmail.com>
2025-08-03 15:46:37 +0900

    [refactor] SubTemplateResponseDTO swagger 날짜 포맷 수정(#40)

--- backend/src/main/java/com/hotpack/krocs/domain/templates/dto/response/SubTemplateResponseDTO.java
+++ backend/src/main/java/com/hotpack/krocs/domain/templates/dto/response/SubTemplateResponseDTO.java
@@ -3,6 +3,7 @@ package com.hotpack.krocs.domain.templates.dto.response;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,10 +22,20 @@ public class SubTemplateResponseDTO {
 
     private final String title;
 
+    @Schema(
+        description = "생성 일시",
+        pattern = "yyyy-MM-dd'T'HH:mm:ss",
+        example = "2025-08-03T15:05:12"
+    )
     @JsonProperty("created_at")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private final LocalDateTime createdAt;
 
+    @Schema(
+        description = "수정 일시",
+        pattern = "yyyy-MM-dd'T'HH:mm:ss",
+        example = "2025-08-03T15:05:12"
+    )
     @JsonProperty("updated_at")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private final LocalDateTime updatedAt;
