0ce90aff391a1a8d4f5589a9ffdd95ebdc78ccde
gumguard <gawonlee0712@gmail.com>
2025-08-03 16:52:01 +0900

    [fix] dto schema에 JsonFormat 추가

--- backend/src/main/java/com/hotpack/krocs/domain/plans/domain/SubPlan.java
+++ backend/src/main/java/com/hotpack/krocs/domain/plans/domain/SubPlan.java
@@ -1,7 +1,15 @@
 package com.hotpack.krocs.domain.plans.domain;
 
 import com.hotpack.krocs.global.common.entity.BaseTimeEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
