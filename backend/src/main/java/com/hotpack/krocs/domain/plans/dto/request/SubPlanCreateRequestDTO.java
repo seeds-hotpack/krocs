213ecfb7bc298f336c519b492cfb853fe20d1bd2
gumguard <gawonlee0712@gmail.com>
2025-08-03 16:18:11 +0900

    [fix] 리뷰 수정 적용 1

--- backend/src/main/java/com/hotpack/krocs/domain/plans/dto/request/SubPlanCreateRequestDTO.java
+++ backend/src/main/java/com/hotpack/krocs/domain/plans/dto/request/SubPlanCreateRequestDTO.java
@@ -2,9 +2,13 @@ package com.hotpack.krocs.domain.plans.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.*;
-
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -12,6 +16,7 @@ import java.util.List;
 @Getter
 public class SubPlanCreateRequestDTO {
 
+    @NotEmpty
     @JsonProperty("sub_plans")
     @Schema(description = "SubPlan 리스트")
     private List<SubPlanRequestDTO> subPlans;
