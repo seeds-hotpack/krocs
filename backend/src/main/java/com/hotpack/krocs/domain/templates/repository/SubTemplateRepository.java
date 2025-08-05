f16db891cdea84adfaddb84544e3ee030d4bb3f2
yeo-li <parkseongyeol2110@gmail.com>
2025-08-05 20:48:00 +0900

    [feat] SubTemplateRepository findByTemplate 추가(#46)

--- backend/src/main/java/com/hotpack/krocs/domain/templates/repository/SubTemplateRepository.java
+++ backend/src/main/java/com/hotpack/krocs/domain/templates/repository/SubTemplateRepository.java
@@ -1,10 +1,13 @@
 package com.hotpack.krocs.domain.templates.repository;
 
 import com.hotpack.krocs.domain.templates.domain.SubTemplate;
+import com.hotpack.krocs.domain.templates.domain.Template;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubTemplateRepository extends JpaRepository<SubTemplate, Long> {
 
+    List<SubTemplate> findByTemplate(Template template);
 }
