59bb6f00299a6e2c32518c4945a41b73614fa121
gumguard <gawonlee0712@gmail.com>
2025-08-05 22:26:02 +0900

    [fix] SubPlanListResponseDTO JsonProperty 수정

--- backend/src/main/java/com/hotpack/krocs/domain/plans/dto/response/SubPlanListResponseDTO.java
+++ backend/src/main/java/com/hotpack/krocs/domain/plans/dto/response/SubPlanListResponseDTO.java
@@ -1,6 +1,8 @@
 package com.hotpack.krocs.domain.plans.dto.response;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,5 +11,7 @@ import lombok.Getter;
 @Getter
 public class SubPlanListResponseDTO {
 
+    @JsonProperty("sub_plans")
+    @Schema(description = "SubPlan 리스트")
     private List<SubPlanResponseDTO> subPlans;
 }
