be1fbe62f2e00e1cf2c2fb062b217404451b5beb
soheeGit <63wlsthgml@gmail.com>
2025-08-05 16:15:08 +0900

    [refector] 리뷰 피드백 반영: Valid 제거

--- backend/src/main/java/com/hotpack/krocs/domain/plans/service/PlanService.java
+++ backend/src/main/java/com/hotpack/krocs/domain/plans/service/PlanService.java
@@ -3,10 +3,9 @@ package com.hotpack.krocs.domain.plans.service;
 import com.hotpack.krocs.domain.plans.dto.request.PlanCreateRequestDTO;
 import com.hotpack.krocs.domain.plans.dto.response.PlanListResponseDTO;
 import com.hotpack.krocs.domain.plans.dto.response.PlanResponseDTO;
-import jakarta.validation.Valid;
 
 public interface PlanService {
-    PlanResponseDTO createPlan(@Valid PlanCreateRequestDTO requestDTO, Long userId, Long goalId);
+    PlanResponseDTO createPlan(PlanCreateRequestDTO requestDTO, Long userId, Long goalId);
 
     PlanListResponseDTO getAllPlans(Long userId);
 
